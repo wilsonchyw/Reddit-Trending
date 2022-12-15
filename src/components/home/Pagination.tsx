0ce8ce50520d6c1a456ccb3dec0ff6f3f5ca9a43
Wilsonych <0913hung@gmail.com>
2022-12-15 02:49:58 +0000

    Fix background color and Add mobile for table

--- src/components/home/Pagination.tsx
+++ src/components/home/Pagination.tsx
@@ -1,21 +1,23 @@
 import { Pagination } from 'react-bootstrap';
+import useAgent from 'src/lib/useAgent';
 export default function Pagination_({ currentPage, total, handlePageChange }) {
+    const isMobile: boolean = useAgent();
     const pageChange = value => {
         if (currentPage + value > total || currentPage + value < 1) return;
         handlePageChange(currentPage + value);
     };
-    //
     if (currentPage > total) handlePageChange(1);
-    //console.log({ currentPage, total })
+    
+    const COUNT = isMobile ? 2 : 4;
+
     return (
-        <Pagination className="mx-auto">
+        <Pagination className="mx-auto" size={'sm'}>
             <Pagination.Prev onClick={() => pageChange(-1)} />
             <Pagination.Item active={currentPage === 1} onClick={() => handlePageChange(1)}>
                 1
             </Pagination.Item>
             {Array.from({ length: total - 2 }, (_, i) => i + 2).map(page => {
-                if (page >= currentPage - 4 && page <= currentPage + 4) {
-                    //|| page >= total - 4 || page <= 3
+                if (page >= currentPage - COUNT && page <= currentPage + COUNT) {
                     return (
                         <Pagination.Item onClick={() => pageChange(page - currentPage)} active={currentPage === page} key={page}>
                             {page}
--- src/components/home/ThreadTrendTable.tsx
+++ src/components/home/ThreadTrendTable.tsx
@@ -7,6 +7,7 @@ import Text from 'src/components/Text';
 import { RootState } from 'src/store';
 import { FONT, FONT_COLOR } from 'src/variables/css';
 import Pagination_ from './Pagination';
+import useAgent from 'src/lib/useAgent';
 
 export default function ThreadTrend({ showForums, handleThreadFetch, userSearch, setUserSearch, target, datas }) {
     const [prePage, setPrePage] = useState<number>(10);
@@ -14,10 +15,11 @@ export default function ThreadTrend({ showForums, handleThreadFetch, userSearch,
     const [sortOrder, setSortOrder] = useState<number>(-1);
     const [currentPage, setCurrent] = useState(1);
     const { dateRange } = useSelector((state: RootState) => state.setting);
+    const isMobile: boolean = useAgent();
 
     if (!datas.length) return <Loading size={300} isWhite={false} />;
 
-    const sortIndicator = (t:string)=> t==sortBy?(sortOrder>0?"⬆":"⬇"):""
+    const sortIndicator = (t: string) => (t == sortBy ? (sortOrder > 0 ? '⬆' : '⬇') : '');
 
     let max = 0;
     const _datas = datas
@@ -39,13 +41,13 @@ export default function ThreadTrend({ showForums, handleThreadFetch, userSearch,
     return (
         <_Card>
             <Stack>
-                <Row className="d-flex justify-content-between m-2">
-                    <Col className="d-flex align-items-center ">
+                <Row className="d-flex justify-content-between p-2">
+                    <Col xs={12} md={4} className="d-flex align-items-center p-1">
                         <Text fontSize={FONT.bigger}>
                             Changes within {dateRange * 24} hours, Total {_datas.length} record
                         </Text>
                     </Col>
-                    <Col className="d-flex align-items-center ">
+                    <Col xs={12} md={4} className="d-flex align-items-center p-1">
                         <InputGroup className="">
                             <Form.Control size="sm" placeholder={userSearch || 'Search'} />
                             <Button
@@ -59,7 +61,7 @@ export default function ThreadTrend({ showForums, handleThreadFetch, userSearch,
                             </Button>
                         </InputGroup>
                     </Col>
-                    <Col className="d-flex align-items-center justify-content-end ">
+                    <Col xs={12} md={4} className="d-flex align-items-center justify-content-md-end justify-content-sm-center p-1">
                         <DropdownButton title={`Page size`} size="sm">
                             <Dropdown.Item onClick={() => setPrePage(10)}>10</Dropdown.Item>
                             <Dropdown.Item onClick={() => setPrePage(25)}>25</Dropdown.Item>
@@ -72,37 +74,140 @@ export default function ThreadTrend({ showForums, handleThreadFetch, userSearch,
                 </div>
             </Stack>
 
-            <Stack style={{ overflowY: 'scroll', minHeight: '300px' }}>
-                <Table>
-                    <thead style={{ backgroundColor: 'rgb(247, 250, 252)' }}>
-                        <tr>
-                            <th className="col-md-7">
-                                <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('title')}>
-                                    Title
-                                </Text>
-                            </th>
-                            <th className="col-md-1">
-                                <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('forum')}>
-                                    Forum{sortIndicator('forum')}
-                                </Text>
-                            </th>
-                            <th className="col-md-1">
-                                <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('MAX')}>
-                                    {target}{sortIndicator('MAX')}
-                                </Text>
-                            </th>
-                            <th className="col-md-1">
-                                <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('change')}>
-                                    Change{sortIndicator('change')}
-                                </Text>
-                            </th>
-                            <th className="col-lg-1  col-md-0 px-0"></th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
+            <Stack style={{  minHeight: '300px' }}>
+                {isMobile ? (
+                    <div>
+                        <Row className="border">
+                            <Row>
+                                <Col>
+                                    <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('title')}>
+                                        Title
+                                    </Text>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={4}>
+                                    <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('forum')}>
+                                        Forum{sortIndicator('forum')}
+                                    </Text>
+                                </Col>
+                                <Col xs={4}>
+                                    <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('MAX')}>
+                                        {target}
+                                        {sortIndicator('MAX')}
+                                    </Text>
+                                </Col>
+                                <Col xs={4}>
+                                    <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('change')}>
+                                        Change{sortIndicator('change')}
+                                    </Text>
+                                </Col>
+                            </Row>
+                        </Row>
                         {_datas.slice((currentPage - 1) * prePage, (currentPage - 1) * prePage + prePage).map((el, index, arr) => (
-                            <tr key={el.id} className="my-2">
+                            <Row key={el.id} className="p-1 border-bottom">
+                                <Row>
+                                    <Col>
+                                        <Text color={FONT_COLOR.darkGrey}>
+                                            <a href={`https://www.reddit.com/r/${el.forum}/comments/${el.id}`} target="_blank" rel="noreferrer">
+                                                {el.title}
+                                            </a>
+                                        </Text>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={5} className="pe-0">
+                                        <Text color={FONT_COLOR.grey} fontWeight={600}>
+                                            {el.forum}
+                                        </Text>
+                                    </Col>
+                                    <Col xs={3} className="p-0">
+                                        <Text color={FONT_COLOR.grey} fontWeight={600}>
+                                            {el.MAX}
+                                        </Text>
+                                    </Col>
+                                    <Col xs={3} className="p-0">
+                                        <Text color={FONT_COLOR.grey} fontWeight={600}>
+                                            &#x2B06;{`${el.change}%`}
+                                        </Text>
+                                    </Col>
+                                    <Col xs={1} className="p-0">
+                                        <Text fontSize={FONT.small}>
+                                            <Badge bg="primary" onClick={() => handleThreadFetch(el.id)}>
+                                                CHART
+                                            </Badge>
+                                        </Text>
+                                    </Col>
+                                </Row>
+                            </Row>
+                        ))}
+                    </div>
+                ) : (
+                    <Table>
+                        <thead style={{ backgroundColor: 'rgb(247, 250, 252)' }}>
+                            <tr>
+                                <th className="col-md-8">
+                                    <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('title')}>
+                                        Title
+                                    </Text>
+                                </th>
+                                <th className="col-md-1">
+                                    <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('forum')}>
+                                        Forum{sortIndicator('forum')}
+                                    </Text>
+                                </th>
+                                <th className="col-md-1">
+                                    <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('MAX')}>
+                                        {target}
+                                        {sortIndicator('MAX')}
+                                    </Text>
+                                </th>
+                                <th className="col-md-1">
+                                    <Text color={FONT_COLOR.darkGrey} handleClick={() => handleSortOrder('change')}>
+                                        Change{sortIndicator('change')}
+                                    </Text>
+                                </th>
+                                <th className="col-lg-1  col-md-0 px-0"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {_datas.slice((currentPage - 1) * prePage, (currentPage - 1) * prePage + prePage).map((el, index, arr) => (
+                                <tr key={el.id} className="my-2">
+                                    <td>
+                                        <Badge bg="primary" onClick={() => handleThreadFetch(el.id)}>
+                                            CHART
+                                        </Badge>{' '}
+                                        <Text color={FONT_COLOR.darkGrey}>
+                                            <a href={`https://www.reddit.com/r/${el.forum}/comments/${el.id}`} target="_blank" rel="noreferrer">
+                                                {el.title}
+                                            </a>
+                                        </Text>
+                                    </td>
+                                    <td>
+                                        <Text color={FONT_COLOR.darkGrey}>{el.forum}</Text>
+                                    </td>
+                                    <td>
+                                        <Text color={FONT_COLOR.darkGrey}>{el.MAX}</Text>
+                                    </td>
+                                    <td>
+                                        <Text color={FONT_COLOR.darkGrey}>&#x2B06;{`${el.change}%`}</Text>
+                                    </td>
+                                    <td className="col-lg-1  col-md-0 px-0">
+                                        <div className="progress p-0 mt-2" style={{ maxHeight: '10px' }}>
+                                            <div className="progress-bar" style={{ width: `${(el.change / max) * 100}%` }}></div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                )}
+            </Stack>
+        </_Card>
+    );
+}
+/**
+ <tr key={el.id} className="my-2">
                                 <td>
                                     <Badge bg="primary" onClick={() => handleThreadFetch(el.id)}>
                                         CHART
@@ -127,11 +232,37 @@ export default function ThreadTrend({ showForums, handleThreadFetch, userSearch,
                                         <div className="progress-bar" style={{ width: `${(el.change / max) * 100}%` }}></div>
                                     </div>
                                 </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </Stack>
-        </_Card>
-    );
-}
+                            </tr>  
+                            
+
+                            <div>
+                    {_datas.slice((currentPage - 1) * prePage, (currentPage - 1) * prePage + prePage).map((el, index, arr) => (
+                        <Row key={el.id} className="m-1 border-bottom">
+                            <Col md={8}>
+                                <Badge bg="primary" onClick={() => handleThreadFetch(el.id)}>
+                                    CHART
+                                </Badge>{' '}
+                                <Text color={FONT_COLOR.darkGrey}>
+                                    <a href={`https://www.reddit.com/r/${el.forum}/comments/${el.id}`} target="_blank" rel="noreferrer">
+                                        {el.title}
+                                    </a>
+                                </Text>
+                            </Col>
+                            <Col md={1}>
+                                <Text color={FONT_COLOR.darkGrey}>{el.forum}</Text>
+                            </Col>
+                            <Col md={1}>
+                                <Text color={FONT_COLOR.darkGrey}>{el.MAX}</Text>
+                            </Col>
+                            <Col md={1}>
+                                <Text color={FONT_COLOR.darkGrey}>&#x2B06;{`${el.change}%`}</Text>
+                            </Col>
+                            <Col md={1} className=" px-0">
+                                <div className="progress p-0 mt-2" style={{ maxHeight: '10px' }}>
+                                    <div className="progress-bar" style={{ width: `${(el.change / max) * 100}%` }}></div>
+                                </div>
+                            </Col>
+                        </Row>
+                    ))}
+                </div>
+                            */
