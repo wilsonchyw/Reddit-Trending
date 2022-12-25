d427f5ac69106e9fe378332125c3bb0ee6749012
Wilsonych <0913hung@gmail.com>
2022-12-25 09:10:46 +0000

    Fix type error

--- src/components/home/ForumBar.tsx
+++ src/components/home/ForumBar.tsx
@@ -8,12 +8,12 @@ export default function Forumbar({ datas }: { datas: State[] }) {
         if (!map[data.forum]) map[data.forum] = 0;
         map[data.forum]++;
     });
-    map = Object.fromEntries(Object.entries(map).sort(([, a], [, b]) => b - a));
+    map = Object.fromEntries(Object.entries(map).sort(([, a]: [string, number], [, b]: [string, number]) => b - a));
 
     const _data = {
         labels: Object.keys(map),
         data: Object.values(map)
     };
 
-    return <Bar data={_data} title={'SubReddit'} subTitle={'24hr'}  color={"#00E396"} />;
+    return <Bar data={_data} title={'SubReddit'} subTitle={'24hr'} color={'#00E396'} />;
 }
